ecc674d3f1b09185cfa77d9e595b4a9773d1cd28
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-06-21 15:13:02 -0500

    Crea tercera tarea en Gulp (Agrega Watch para hacer seguimiento a los cambios de los archivos SCSS)

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp .task( 'sass', function() {
 
 /* Creamos la 2da Tarea:
    1. Asigna un nombre a la tarea "serve"
-   2. Asigna el array con el conjunto de tareas que se van a ejecutar previamente.
+   2. Asigna el array con el conjunto de tareas que se van a ejecutar previamente "sass".
    3. Define las acciones de la tarea  */
 gulp .task( 'serve', [ 'sass' ], function() {
   /* a. Inicializa el servidor, indicandole los archivos a los que les va a hacer seguimiento de cambios  */
@@ -52,4 +52,20 @@ gulp .task( 'serve', [ 'sass' ], function() {
     $ gulp serve
 
     Que se encargará a su ves de lanzar la tarea "sass"
+*/
+
+/* Creamos la 3ra Tarea:
+   1. Asigna un nombre a la tarea "watch"
+   2. Asigna el array con el conjunto de tareas que se van a ejecutar previamente "sass" y "serve".
+   3. Define las acciones de la tarea  */
+gulp .task( 'watch', [ 'sass', 'serve' ], function() {
+  /* a. Ejecuta la tarea "sass" y aplica el seguimiendo de la misma al directorio "scss" y todos los archivos con extensión ".scss" */
+  gulp .watch( [ 'scss/*.scss' ], [ 'sass' ] );
+});
+
+/* NOTA:
+    Para lanzar esta tarea usamos el comando gulp y el nombre de la tarea.
+    $ gulp watch
+
+    Que se encargará a su ves de lanzar la tarea "sass" y la tarea "serve" previamente
 */
